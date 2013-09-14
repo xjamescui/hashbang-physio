ecf3481e1361a623756f27d4a7ceb1acb805324a
James Cui <james.q.cui@gmail.com>
2013-09-13 21:48:56 -0400

    basic element id labelling completed

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,7 +3,9 @@
 /* Controllers */
 
 angular.module('myApp.controllers', []).
-  controller('MyCtrl1', [function() {
+  controller('MyCtrl1', ['$scope',function(scope) {
+  	scope.hi = "hi";
+  	scope.exercises = ["shoulder rotate", "arm raise"];
 
   }])
   .controller('MyCtrl2', [function() {
