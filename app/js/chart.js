78fc280fd08303fcd15d2f6d5944ad35f5d86de9
James Cui <james.q.cui@gmail.com>
2013-09-13 23:29:28 -0400

    chart

--- app/js/chart.js
+++ app/js/chart.js
@@ -1,24 +1,28 @@
-var data = {
-	labels : ["January","February","March","April","May","June","July"],
-	datasets : [
-		{
-			fillColor : "rgba(220,220,220,0.5)",
-			strokeColor : "rgba(220,220,220,1)",
-			pointColor : "rgba(220,220,220,1)",
-			pointStrokeColor : "#fff",
-			data : [65,59,90,81,56,55,40]
-		},
-		{
-			fillColor : "rgba(151,187,205,0.5)",
-			strokeColor : "rgba(151,187,205,1)",
-			pointColor : "rgba(151,187,205,1)",
-			pointStrokeColor : "#fff",
-			data : [28,48,40,19,96,27,100]
-		}
-	]
-}
-
-
 //Get the context of the canvas element we want to select
-var ctx = document.getElementById("myChart").getContext("2d");
-var myNewChart = new Chart(ctx).Line(data);
+var ctx = $("#chart").get(0).getContext("2d");
+// console.log(chart);
+
+if (ctx != undefined) {
+	// console.log(chart);
+
+	// var ctx = chart.getContext("2d");
+	var data = {
+		labels: ["January", "February", "March", "April", "May", "June", "July"],
+		datasets: [{
+			fillColor: "rgba(220,220,220,0.5)",
+			strokeColor: "rgba(220,220,220,1)",
+			pointColor: "rgba(220,220,220,1)",
+			pointStrokeColor: "#fff",
+			data: [65, 59, 90, 81, 56, 55, 40]
+		}, {
+			fillColor: "rgba(151,187,205,0.5)",
+			strokeColor: "rgba(151,187,205,1)",
+			pointColor: "rgba(151,187,205,1)",
+			pointStrokeColor: "#fff",
+			data: [28, 48, 40, 19, 96, 27, 100]
+		}]
+	};
+
+	new Chart(ctx).Line(data);
+	alert("chart is ready");
+}
