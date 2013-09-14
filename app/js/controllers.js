11f3bf9ffad068f0fcc487b6c8ef711dd7686718
James Cui <james.q.cui@gmail.com>
2013-09-14 05:36:39 -0400

    fdssdf

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -7,27 +7,42 @@ controller('patientCtrl', ['$scope', '$http',
 	function($scope, $http) {
 		$scope.identity = "Patient";
 		$scope.exercises = ["shoulder rotate", "arm raise"];
-
+		$scope.total_set = 3;
 		$scope.init = function() {
 
 
-			// $http.get('http://api.geonames.org/citiesJSON?north=44.1&south=-9.9&east=-22.4&west=55.2&lang=de&username=demo').
-			// success(function(data, status, headers, config) {
-			// 	// this callback will be called asynchronously
-			// 	// when the response is available
-			// 	$scope.data = data;
-			// }).
-			// error(function(data, status, headers, config) {
-			// 	// called asynchronously if an error occurs
-			// 	// or server returns response with an error status.
-			// 	console.log('get error');
-			// });
-		}
+			$http.get('js/data.json').
+			success(function(data, status, headers, config) {
+				// this callback will be called asynchronously
+				// when the response is available
+
+				$scope.shoulder_rotation_count = data.shoulder_rotate.set_count;
+			}).
+			error(function(data, status, headers, config) {
+				// called asynchronously if an error occurs
+				// or server returns response with an error status.
+				console.log('get error');
+			});
+		};
 
 	}
 ])
-	.controller('physioCtrl', ['$scope',
-		function($scope) {
-			$scope.identity= "Physiotherapist";
+	.controller('physioCtrl', ['$scope', '$http',
+		function($scope, $http) {
+			$scope.identity = "Physiotherapist";
+
+			$http.get('js/data.json').
+			success(function(data, status, headers, config) {
+				// this callback will be called asynchronously
+				// when the response is available
+				$scope.arm_raise_count = data.arm_raise.set_count;
+				$scope.shoulder_rotate_count = data.shoulder_rotate.set_count;
+			}).
+			error(function(data, status, headers, config) {
+				// called asynchronously if an error occurs
+				// or server returns response with an error status.
+				console.log('get error');
+			});
+
 		}
 	]);
